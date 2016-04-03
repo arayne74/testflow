345914a7128917264d5b85e9f90ed81a95eba60a
Andy Rayne <andy_rayne@arum.co.uk>
2016-04-03 20:05:03 +0100

    Adding initial contents of project to the repo

---
+++ src/com/arayne/beagit/BeAGit.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.arayne.beagit;
+
+/**
+ * @author andy_rayne
+ *
+ */
+
+
+public class BeAGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args )
+	{
+		// TODO Auto-generated method stub
+		System.out.println( "Gitting it done" );
+	}
+
+}
